9d0a1480c44ffeb6fee9d2c37dabeefaf3af6e1b
TadaoYamaoka <tadaoyamaoka@gmail.com>
2023-11-06 21:27:45 +0900

    Add collect_rollouts

---
+++ collect_rollouts/collect_rollouts.cpp
@@ -0,0 +1,20 @@
+﻿#include "../../cmajiang/src_cpp/game.h"
+
+#include "cxxopts.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    cxxopts::Options options("collect_rollouts", "Collect rollouts");
+
+    const auto result = options.parse(argc, argv);
+
+    Game game;
+    game.kaiju();
+    game.qipai();
+
+    std::cout << game.shoupai_(0).toString() << std::endl;
+
+    return 0;
+}
